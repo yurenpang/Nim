60e377f8aeb54961855ad8e47c4b108803df2678
paranoiark <paranoiark@outlook.com>
2018-12-02 23:41:24 -0600

    Heap created.

--- src/Heap.java
+++ src/Heap.java
@@ -1,2 +1,29 @@
-public class Heap {
+import comp124graphics.Ellipse;
+import comp124graphics.GraphicsGroup;
+import comp124graphics.Rectangle;
+
+public class Heap extends GraphicsGroup {
+    private double width;
+    private int id;
+    private int numBean;
+
+    public Heap(double x, double y, double heapLength, int id){
+        super(x,y);
+        this.width=heapLength;
+        this.id=id;
+        createHeap(x,y);
+    }
+
+    private void createHeap(double x, double y){
+        Rectangle heap = new Rectangle(x, y, width, width);
+        add(heap);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
