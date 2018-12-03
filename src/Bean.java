2ba6ce91e54964bdcfa5cf83b8a1aa2f271e6064
yurenpang <ypang@macalester.edu>
2018-12-03 15:37:30 -0600

    Bean extends Ellipse

--- src/Bean.java
+++ src/Bean.java
@@ -1,20 +1,13 @@
 import comp124graphics.Ellipse;
-import comp124graphics.GraphicsGroup;
 
-public class Bean extends GraphicsGroup {
+public class Bean extends Ellipse {
     public static final double RADIUS_LENGTH = 20;
 
     private int id;
 
     public Bean(double x, double y, int id){
-        super(x,y);
+        super(x, y, RADIUS_LENGTH, RADIUS_LENGTH);
         this.id = id;
-        createBean();
-    }
-
-    private void createBean(){
-        Ellipse bean = new Ellipse(0, 0, RADIUS_LENGTH, RADIUS_LENGTH);
-        add(bean);
     }
 
     public void setId(int id) {
