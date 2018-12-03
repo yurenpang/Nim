45a24480087d49f6db6af907a08660ad518596f2
yurenpang <ypang@macalester.edu>
2018-12-02 23:55:39 -0600

    Created Bean

---
+++ src/Bean.java
@@ -0,0 +1,23 @@
+import comp124graphics.Ellipse;
+import comp124graphics.GraphicsGroup;
+
+public class Bean extends GraphicsGroup {
+    private static final double RADIUS_LENGTH = 20;
+
+    private int id;
+
+    public Bean(double x, double y, int id){
+        super(x,y);
+        this.id = id;
+        createBean();
+    }
+
+    private void createBean(){
+        Ellipse bean = new Ellipse(0, 0, RADIUS_LENGTH, RADIUS_LENGTH);
+        add(bean);
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
